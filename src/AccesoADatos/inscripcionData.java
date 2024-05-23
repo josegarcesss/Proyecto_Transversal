5b1b66bc5c924594298ab2bc9b72358ef392deb4
Santiago Benenatti <sbenenatti@gmail.com>
2024-05-23 10:41:45 -0300

    obtenerInscripciones

--- src/AccesoADatos/inscripcionData.java
+++ src/AccesoADatos/inscripcionData.java
@@ -117,4 +117,24 @@ public class InscripcionData {
         return alumnos;
     }
     
+    public List<Inscripcion> obtenerInscripciones() {
+        List<Inscripcion> inscripciones = new ArrayList<>();
+        String sql= "SELECT * FROM inscripcion";
+        try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Inscripcion inscripcion = new Inscripcion();
+                ps.setInt(1, inscripcion.getId_Inscripcion());
+                ps.setDouble(2,inscripcion.getNota());
+                inscripcion.setAlumno(alumData.buscarAlumno(rs.getInt("id_alumno")));
+                inscripcion.setMateria(matData.buscarMateria(rs.getInt("id_materia")));
+                inscripciones.add(inscripcion);
+            }
+            ps.close();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error al obtener las inscripciones");
+            }
+        return inscripciones;
+    }
 }
